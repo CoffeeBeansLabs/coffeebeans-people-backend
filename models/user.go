6de2b39235913115d221156ea15244a9ea598d41
set <ayush@coffeebeans.io>
2020-07-05 16:59:17 +0530

    Ayush : Update json field name to skil

--- models/user.go
+++ models/user.go
@@ -13,7 +13,7 @@ type User struct {
 	Designation       string             `json:"designation,omitempty" bson:"designation,omitempty"`
 	DOJ               string             `json:"doj,omitempty" bson:"doj,omitempty"`
 	DOB               string             `json:"dob,omitempty" bson:"dob,omitempty"`
-	Skill             string             `json:"skills,omitempty" bson:"skills,omitempty"`
+	Skill             string             `json:"skill,omitempty" bson:"skill,omitempty"`
 	YearsOfExp        string             `json:"experience,omitempty" bson:"experience,omitempty"`
 	Id                primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	AddtitionalDetail AdditionalDetail   `json:"additiona_detail,omitempty" bson:"additional_detail,omitempty"`
